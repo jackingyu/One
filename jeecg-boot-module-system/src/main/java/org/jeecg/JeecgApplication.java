28baccbe7fa8dbec118059415a2b38ed3be894c4
jack yu <jack.yu06@sap.com>
2020-02-15 20:30:23 +0800

    support swagger document on all the projects

--- jeecg-boot-module-system/src/main/java/org/jeecg/JeecgApplication.java
+++ jeecg-boot-module-system/src/main/java/org/jeecg/JeecgApplication.java
@@ -3,16 +3,12 @@ package org.jeecg;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-import org.apache.catalina.Context;
-import org.apache.tomcat.util.scan.StandardJarScanner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Slf4j
 @EnableSwagger2
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.elnido","org.jeecg"})
 public class JeecgApplication extends SpringBootServletInitializer {
 
     @Override
--- jeecg-boot-module-system/src/main/java/org/jeecg/config/Swagger2Config.java
+++ jeecg-boot-module-system/src/main/java/org/jeecg/config/Swagger2Config.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import io.swagger.annotations.Api;
 import org.jeecg.modules.shiro.vo.DefContants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -60,7 +61,7 @@ public class Swagger2Config implements WebMvcConfigurer {
 				.apiInfo(apiInfo())
 				.select()
 				//此包路径下的类，才生成接口文档
-				.apis(RequestHandlerSelectors.basePackage("org.jeecg.modules"))
+				.apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
 				//加了ApiOperation注解的类，才生成接口文档
 	            .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
 				.paths(PathSelectors.any())
